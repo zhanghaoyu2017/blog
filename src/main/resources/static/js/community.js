7fc84d63d8614749066725a19a6117d4962f16bb
zhanghaoyu2017 <273903932@qq.com>
2020-01-21 10:55:39 +0800

    初步完成回复

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,38 @@
+function post() {
+    var questionId=$("#question_id").val();
+    var content = $("#comment_content").val();
+    comment2target(questionId, 1, content);
+}
+function comment2target(targetId, type, content) {
+    if (!content) {
+        alert("不能回复空内容~~~");
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": targetId,
+            "content": content,
+            "type": type
+        }),
+        success: function (response) {
+            if (response.code == 200) {
+                window.location.reload();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=032b5548086efe9c0ec0&amp;redirect_uri=" + document.location.origin + "/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
+        },
+        dataType: "json"
+    });
+}
